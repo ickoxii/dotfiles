e38c89c3e882f8ae31a90fd658149db7d33cf3a1
Icko <80863308+ickoxii@users.noreply.github.com>
2023-09-05 21:15:09 -0500

    create symlink script automated and organized

--- create-sym-link.sh
+++ create-sym-link.sh
@@ -1,33 +1,105 @@
 #!/opt/homebrew/bin/bash
 
-# information on 'ln' can be found in /documents/coding/notes
+# Loop to create symlink
 
-# GENERAL TERMINAL
-ln -s $HOME/.dotfiles/.profile $HOME/.profile
-ln -s $HOME/.dotfiles/.hushlogin $HOME/.hushlogin
+create_symlink() {
+  local src=$1
+  local dest=$2
 
-# BASH
-ln -s $HOME/.dotfiles/bash/.bash_aliases $HOME/.bash_aliases
-ln -s $HOME/.dotfiles/bash/.bash_profile $HOME/.bash_profile
-ln -s $HOME/.dotfiles/bash/.bash_prompt $HOME/.bash_prompt
-ln -s $HOME/.dotfiles/bash/.bashrc $HOME/.bashrc
+  if [[ -L $dest ]]; then # Check if a symlink exists at the destination
+    if [[ ! -e $dest ]]; then # Check if the symlink is dangling
+      read -p "$dest is a dangling symlink. Do you want to remove and recreate it? [y/n] " choice
+      case $choice in
+        y|Y)
+          rm $dest && ln -s $src $dest
+          echo "Recreated symlink from $src to $dest"
+          ;;
+        *)
+          echo "Skipped $dest"
+          ;;
+      esac
+    else
+      echo "$dest symlink already exists and is not dangling. Skipped."
+    fi
+  else
+    ln -s $src $dest && echo "Created symlink from $src to $dest"
+  fi
+}
 
-# ZSH
-ln -s $HOME/.dotfiles/zsh/.zprofile $HOME/.zprofile
-ln -s $HOME/.dotfiles/zsh/.z_aliases $HOME/.zsh_aliases
-ln -s $HOME/.dotfiles/zsh/.z_prompt $HOME/.zsh_prompt
-ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+# Declare associative arrays for each application
 
-# GIT
-ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+declare -A GENERAL_TERMINAL=(
+  ["$HOME/.dotfiles/.profile"]="$HOME/.profile"
+  ["$HOME/.dotfiles/.hushlogin"]="$HOME/.hushlogin"
+)
 
-# VIM
-ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+declare -A BASH=(
+  ["$HOME/.dotfiles/bash/.bash_aliases"]="$HOME/.bash_aliases"
+  ["$HOME/.dotfiles/bash/.bash_profile"]="$HOME/.bash_profile"
+  ["$HOME/.dotfiles/bash/.bash_prompt"]="$HOME/.bash_prompt"
+  ["$HOME/.dotfiles/bash/.bashrc"]="$HOME/.bashrc"
+)
 
-# NEOFETCH
-ln -s $HOME/.dotfiles/neofetch/config.conf $HOME/.config/neofetch/config.conf
+declare -A ZSH=(
+    ["$HOME/.dotfiles/zsh/.zprofile"]="$HOME/.zprofile"
+    ["$HOME/.dotfiles/zsh/.z_aliases"]="$HOME/.zsh_aliases"
+    ["$HOME/.dotfiles/zsh/.z_prompt"]="$HOME/.zsh_prompt"
+    ["$HOME/.dotfiles/zsh/.zshrc"]="$HOME/.zshrc"
+)
 
-# SPOTIFY CLI
-ln -s $HOME/.dotfiles/spotifyd/spotifyd.conf $HOME/.config/spotifyd/spotifyd.conf
-ln -s $HOME/.dotfiles/spotify-tui/config.yml $HOME/.config/spotify-tui/config.yml
+declare -A GIT=(
+    ["$HOME/.dotfiles/.gitconfig"]="$HOME/.gitconfig"
+)
+
+declare -A VIM=(
+    ["$HOME/.dotfiles/vim/.vimrc"]="$HOME/.vimrc"
+    ["$HOME/.dotfiles/vim/autocommands.vim"]="$HOME/.vim/autocommands.vim"
+    ["$HOME/.dotfiles/vim/vim-templates/gitignore-template"]="$HOME/.vim/templates/gitignore-template"
+)
+
+declare -A NEOFETCH=(
+    ["$HOME/.dotfiles/neofetch/config.conf"]="$HOME/.config/neofetch/config.conf"
+)
+
+declare -A SPOTIFYCLI=(
+    ["$HOME/.dotfiles/spotifyd/spotifyd.conf"]="$HOME/.config/spotifyd/spotifyd.conf"
+    ["$HOME/.dotfiles/spotify-tui/config.yml"]="$HOME/.config/spotify-tui/config.yml"
+)
+
+# Loops to actually run the create_symlink function on all files
+
+echo "Linking GENERAL TERMINAL config files..."
+for src in "${!GENERAL_TERMINAL[@]}"; do
+  create_symlink "$src" "${GENERAL_TERMINAL[$src]}"
+done
+
+echo "Linking BASH config files..."
+for src in "${!BASH[@]}"; do
+  create_symlink "$src" "${BASH[$src]}"
+done
+
+echo "Linking ZSH config files..."
+for src in "${!ZSH[@]}"; do
+    create_symlink "$src" "${ZSH[$src]}"
+done
+
+echo "Linking GIT config files..."
+for src in "${!GIT[@]}"; do
+    create_symlink "$src" "${GIT[$src]}"
+done
+
+echo "Linking VIM config files..."
+for src in "${!VIM[@]}"; do
+  create_symlink "$src" "${VIM[$src]}"
+done
+
+echo "Linking NEOFETCH config files..."
+for src in "${!NEOFETCH[@]}"; do
+  create_symlink "$src" "${NEOFETCH[$src]}"
+done
+
+echo "Linking SPOTIFYCLI config files..."
+for src in "${!SPOTIFYCLI[@]}"; do
+  create_symlink "$src" "${SPOTIFYCLI[$src]}"
+done
 
