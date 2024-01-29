0029fb87b7e82255f94a084073cf288562bc2c2d
Icko <80863308+ickoxii@users.noreply.github.com>
2024-01-29 14:24:59 -0600

    (skel-files) added cpp project template

---
+++ skeleton-files/cppTemplate.cpp
@@ -0,0 +1,38 @@
+ /**
+  * proj_desc
+  */
+#include <iostream>
+#include <cstdlib>
+
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <cmath>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+    public:
+        void fun() {
+        }
+};
+
+/*
+class SolutionTest : public Solution {
+};
+*/
+
+ /**
+  * main
+  * 
+  * Grabs input cases and tests
+  */
+int main(int argc, char *argv[]) {
+    return 0;
+}
