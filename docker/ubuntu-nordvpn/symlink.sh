9ed30da4999bda0d34513b0c030c4e84465d66bf
Icko Iben <ickoxii@Ickos-Mac.local>
2024-03-21 00:10:17 -0500

    spring cleaning

--- docker/ubuntu-nordvpn/symlink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-#
-# Symlink for ubuntu-nordvpn
-
-ln -s $HOME/.dotfiles/docker/ubuntu-nordvpn/Dockerfile $HOME/Coding/projects/my-vpn/Dockerfile
--- docker/ubuntu-valgrind/symlink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-#
-# Ubuntu-valgrind Dockerfile symlink
-
-ln -s $HOME/.dotfiles/docker/ubuntu-valgrind/Dockerfile $HOME/Documents/baylor/classes/csi3334/projects/Dockerfile
