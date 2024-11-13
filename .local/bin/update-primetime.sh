6e46b88267626b9f43bbc786efc34a3a42d1a536
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-11-13 04:02:21 -0600

    cron jobs for primetime

--- .local/bin/update-primetime.sh
+++ .local/bin/update-primetime.sh
@@ -2,13 +2,12 @@
 
 project="${HOME}/baylor/projects/PrimeTimePets"
 
+echo -e "Log for $(date '+%Y-%m-%d %H:%M:%S')"
+
 cd ${project} || exit
 
 current_branch=$(git branch --show-current)
 branches=$(git --no-pager branch | grep -v 'main' | sed "s/\*//" | awk '{print $1}')
-
-echo $branches
-echo $current_branch
 
 # Stash current changes
 git stash
@@ -17,15 +16,20 @@ git checkout main
 git fetch --all && git pull origin main
 
 for branch in ${branches}; do
+    echo -e "\033[0;34mChecking out into ${branch}\033[0m"
     git checkout ${branch}
     git stash
-    if ! git merge main; then
+    if git merge main; then
+        echo -e "  \033[0;32mMerged main into branch: ${branch}\033[0m"
+    else
         git merge --abort
-        osascript -e "display dialog \"Merge failed for branch: ${branch}\"" &
+        echo -e "  \033[0;31mMerge failed for branch: ${branch}\033[0m"
     fi
     git stash pop
 done
 
 git stash pop
 
+echo -e "Going back to branch ${current_branch}"
 git checkout ${current_branch}
+echo "--------------------"
