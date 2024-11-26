ffd0804e4b434971e36ffa69aa7b580e3507e65b
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-11-25 18:27:54 -0800

    commenting for plugin keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -11,19 +11,32 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
+        -- [p]roject [f]iles
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+        -- [g]it file [s]earch
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+        -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+        -- [p]roject [w]ith [s]tring (?)
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
+
+        -- [p]roject [W]ith [s]tring
+        -- honextly i dont know the fucking difference between this and the last one
         vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
+
+        -- [p]roject [s]tring search
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
+
+        -- [v]iew [h]elp
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -6,15 +6,16 @@ return {
                 icons = false,
             })
 
+            -- [T]rouble [T]oggle
             vim.keymap.set("n", "<leader>tt", function()
                 require("trouble").toggle()
             end)
 
-            vim.keymap.set("n", "[t", function()
+            vim.keymap.set("n", "]t", function()
                 require("trouble").next({skip_groups = true, jump = true});
             end)
 
-            vim.keymap.set("n", "]t", function()
+            vim.keymap.set("n", "[t", function()
                 require("trouble").previous({skip_groups = true, jump = true});
             end)
 
