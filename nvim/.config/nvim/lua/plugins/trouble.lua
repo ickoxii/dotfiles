1b6f8e20fd648cbea26193d43328eee844c8165e
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-12-13 20:45:14 -0600

    trouble fix

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,23 +1,29 @@
 return {
   {
     "folke/trouble.nvim",
-    config = function()
-      require("trouble").setup({
-        icons = false,
-      })
-
-      -- [T]rouble [T]oggle
-      vim.keymap.set("n", "<leader>tt", function()
-        require("trouble").toggle()
-      end)
-
-      vim.keymap.set("n", "]t", function()
-        require("trouble").next({ skip_groups = true, jump = true });
-      end)
-
-      vim.keymap.set("n", "[t", function()
-        require("trouble").previous({ skip_groups = true, jump = true });
-      end)
-    end
+    opts = {}, -- use default options
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>tt",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tT",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tl",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>tq",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
 }
