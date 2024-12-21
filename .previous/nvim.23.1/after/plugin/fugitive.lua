df1ed423a8c78c979054679e20175d130d35c0ce
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-12-21 08:02:56 -0800

    byebye previous

--- .previous/nvim.23.1/after/plugin/fugitive.lua
+++
@@ -1,2 +0,0 @@
--- [G]et [S]tatus
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
--- .previous/nvim.23.2/lua/ickoxii/init.lua
+++
@@ -1,5 +0,0 @@
-require("ickoxii.options") -- Load leader key changes before lazy
-require("ickoxii.lazy")
-require("ickoxii.remaps")
-require("ickoxii.pandoc")
--- vim.cmd(":Copilot disable") -- Fucking slow
